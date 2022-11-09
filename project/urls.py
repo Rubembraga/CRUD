9413f8932715d540689713d8d976bcb19d2a49ab
Rubem Braga <rubembragajr@gmail.com>
2022-11-09 14:40:26 -0300

    Adicionando o READ

--- project/urls.py
+++ project/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import path
-from app.views import home, form
+from app.views import home, form, create, view
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('form/', form),
+    path('', home, name='home'),
+    path('form/', form, name='form'),
+    path('create/', create, name='create'),
+    path('view/<int:pk>/', view, name='view'),
 ]
